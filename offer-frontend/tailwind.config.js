77377976950e658af908a55d3595386ecf183c47
FannyNh <thethirdmoira@gmail.com>
2026-09-22 20:33:58 +0200

    wip hosting frontend

--- offer-frontend/tailwind.config.js
+++ offer-frontend/tailwind.config.js
@@ -85,6 +85,5 @@ export default {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/line-clamp'),
     ],
 }
